1709ec8f45132afe796cab7e9bbba4a3ffa69a82
Mihai <mihaicosminrosu@yahoo.com>
2023-05-19 21:07:17 +0300

    Implemented singleplayer easy difficulty

--- proiect/macros.h
+++ proiect/macros.h
@@ -8,3 +8,14 @@
 #define LEFTDOWN 7
 #define DOWN 8
 #define RIGHTDOWN 9
+
+#define MENU 0
+#define SP_MENU 1
+#define GAME 2
+#define FIN 3
+
+#define EASY 0
+#define HARD 1
+
+#define SINGLE 0
+#define MULTI 1
--- proiect/proiect.ino
+++ proiect/proiect.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Arduino_ST7789_Fast.h>
-#include "placements.h"
+#include "macros.h"
 
 #define SCR_SIZE   240
 #define TFT_DC  7
@@ -14,8 +14,11 @@ char bigBoard[3][3];
 volatile int currentBox = -1;
 volatile int placement = ALL;
 volatile char player = 'X';
-volatile int state = 0;
+volatile int state = MENU;
+volatile int gameMode = MULTI;
+volatile int difficulty = EASY;
 volatile int moveCount = 0;
+volatile bool hasWon = false;
 
 volatile long lastDebounceTime1 = 0;
 volatile long lastDebounceTime0 = 0;
@@ -24,13 +27,21 @@ volatile long debounceDelay = 50;
 volatile int currentTimerCount = 0;
 volatile int stopTimerCount = 0;
 
+/*    INTERRUPTS    */
 ISR(INT0_vect) {
   if (millis() - lastDebounceTime0 > debounceDelay) {
-    if (state == 0) {
-      Serial.println("Singleplayer");
-    } else if (state == 2) {
-      if (placeValue(player, currentBox)) 
+    if (state == MENU) {
+      drawSingleplayerMenu();
+    } else if (state == GAME) {
+      if (placeValue(player, currentBox)) {
         player = (player == 'X' ? 'O' : 'X');
+        if (gameMode == SINGLE && !hasWon) {
+          makeMoveAI();
+        }
+      }
+    } else if (state == SP_MENU) {
+      difficulty = EASY;
+      startGame();
     } else {
       drawMenu();
     }
@@ -41,8 +52,11 @@ ISR(INT0_vect) {
 
 ISR(INT1_vect) {
   if (millis() - lastDebounceTime1 > debounceDelay) {
-    if (state == 0) {
-      state = 2;
+    if (state == MENU) {
+      gameMode = MULTI;
+      startGame();
+    } else if (state == SP_MENU) {
+      difficulty = HARD;
       startGame();
     } else {
       drawMenu();
@@ -62,18 +76,24 @@ ISR(TIMER1_OVF_vect) {
   }
 }
 
-void drawBoard() {
-  tft.fillScreen(BLACK);
-  for (int p = 12; p < SCR_SIZE; p += 24) {
-    uint16_t color = RGBto565(128, 128, 128);
-    tft.drawLine(p, 12, p, 228, color);
-    tft.drawLine(12, p, 228, p, color);
-  }
+/*    GAME LOGIC    */
+void startGame() {
+  state = GAME;
+  currentBox = -1;
+  placement = ALL;
+  player = 'X';
+  moveCount = 0;
+  hasWon = false;
 
-  for (int p = 12; p < SCR_SIZE; p += 72) {
-    tft.drawLine(p, 12, p, 228, WHITE);
-    tft.drawLine(12, p, 228, p, WHITE);
-  }
+  for (int i = 0; i < 9; i++)
+    for (int j = 0; j < 9; j++)
+      board[i][j] = '_';
+
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      bigBoard[i][j] = '_';
+  
+  drawBoard();
 }
 
 bool canPlace(int boxNumber) {
@@ -132,47 +152,6 @@ void updatePlacement(int boxNumber) {
         placement = RIGHTDOWN;
     }
   }
-}
-
-void drawWinnerSmall(int x, int y, char value) {
-  int xPos = 12 + x * 24;
-  int yPos = 12 + y * 24;
-  tft.fillRect(xPos + 1, yPos + 1, 71, 71, BLACK);
-
-  xPos += 8;
-  yPos += 5;
-  tft.setTextColor(GREEN);
-  if (value == 'X') {
-    tft.setCursor(xPos, yPos);
-    tft.println(value);
-    tft.setCursor(xPos + 48, yPos);
-    tft.println(value);
-    tft.setCursor(xPos + 24, yPos + 24);
-    tft.println(value);
-    tft.setCursor(xPos, yPos + 48);
-    tft.println(value);
-    tft.setCursor(xPos + 48, yPos + 48);
-    tft.println(value);
-  } else {
-    tft.setCursor(xPos, yPos);
-    tft.println(value);
-    tft.setCursor(xPos + 24, yPos);
-    tft.println(value);
-    tft.setCursor(xPos + 48, yPos);
-    tft.println(value);
-    tft.setCursor(xPos, yPos + 24);
-    tft.println(value);
-    tft.setCursor(xPos + 48, yPos + 24);
-    tft.println(value);
-    tft.setCursor(xPos, yPos + 48);
-    tft.println(value);
-    tft.setCursor(xPos + 24, yPos + 48);
-    tft.println(value);
-    tft.setCursor(xPos + 48, yPos + 48);
-    tft.println(value);
-  }
-
-  tft.setTextColor(WHITE);
 }
 
 bool canWinBig(char value) {
@@ -249,8 +228,136 @@ bool placeValue(char value, int boxNumber) {
   return false;
 }
 
+/*    SINGLEPLAYER AI     */
+int getStartSmallBoard(int place) {
+  place -= 1;
+  int x = (place % 3) * 3;
+  int y = (place / 3) * 9;
+  return y * 3 + x;
+}
+
+bool emptyBoard(char localBoard[3][3]) {
+  for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        if (localBoard[i][j] != '_')
+          return false;
+  
+  return true;
+}
+
+int countInBoard(char localBoard[3][3], char value) {
+  int cnt = 0;
+  for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        if (localBoard[i][j] == value)
+          cnt++;
+  
+  return cnt;
+}
+
+bool canWinAI(char localBoard[3][3], char value) {
+  int x = 0;
+  int y = 0;
+  return (localBoard[y][x] == value && localBoard[y][x] == localBoard[y+1][x] && localBoard[y][x] == localBoard[y+2][x])
+      || (localBoard[y][x+1] == value && localBoard[y][x+1] == localBoard[y+1][x+1] && localBoard[y][x+1] == localBoard[y+2][x+1])
+      || (localBoard[y][x+2] == value && localBoard[y][x+2] == localBoard[y+1][x+2] && localBoard[y][x+2] == localBoard[y+2][x+2])
+      || (localBoard[y][x] == value && localBoard[y][x] == localBoard[y][x+1] && localBoard[y][x] == localBoard[y][x+2])
+      || (localBoard[y+1][x] == value && localBoard[y+1][x] == localBoard[y+1][x+1] && localBoard[y+1][x] == localBoard[y+1][x+2])
+      || (localBoard[y+2][x] == value && localBoard[y+2][x] == localBoard[y+2][x+1] && localBoard[y+2][x] == localBoard[y+2][x+2])
+      || (localBoard[y][x] == value && localBoard[y][x] == localBoard[y+1][x+1] && localBoard[y][x] == localBoard[y+2][x+2])
+      || (localBoard[y][x+2] == value && localBoard[y][x+2] == localBoard[y+1][x+1] && localBoard[y][x+2] == localBoard[y+2][x]);
+}
+
+int movePriorityEasy(char localBoard[3][3], int i, int j) {
+  if (localBoard[i][j] != '_')
+    return 99;
+  
+  char oppositePlayer = (player == 'X' ? 'O' : 'X');
+  char secondLocalBoard[3][3];
+  for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        secondLocalBoard[i][j] = localBoard[i][j];
+  
+  secondLocalBoard[i][j] = player;
+  if (canWinAI(secondLocalBoard, player))
+    return 0;
+  
+  secondLocalBoard[i][j] = oppositePlayer;
+  if (canWinAI(secondLocalBoard, oppositePlayer))
+    return 1;
+  
+  if (emptyBoard(localBoard) && ((i == 0 || i == 2) && (j == 0 || j == 2)))
+    return 2;
+  
+  if (countInBoard(localBoard, player) == 0) {
+    if (localBoard[1][1] == oppositePlayer) {
+      if ((i == 0 || i == 2) && (j == 0 || j == 2))
+        return 3;
+    } else {
+      if (i == 1 && j == 1)
+        return 3;
+    }
+  }
+
+  secondLocalBoard[i][j] = player;
+  int box = findBestEasy(secondLocalBoard);
+  if (movePriorityEasy(secondLocalBoard, box / 3, box % 3) == 0)
+    return 4;
+  
+  if ((i == 0 || i == 2) && (j == 0 || j == 2))
+    return 5;
+  
+  return 6;
+}
+
+int findBestEasy(char localBoard[3][3]) {
+  int bestMove = 99, bestI = 0, bestJ = 0;
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++) {
+      int currentMove = movePriorityEasy(localBoard, i, j);
+      if (currentMove < bestMove) {
+        bestMove = currentMove;
+        bestI = i;
+        bestJ = j;
+      }
+    }
+  
+  return bestI * 3 + bestJ;
+}
+
+int searchMoveEasy() {
+  int localPlacement = placement;
+
+  if (placement == ALL) {
+    char localBigBoard[3][3];
+    for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        localBigBoard[i][j] = bigBoard[i][j];
+    
+    localPlacement = findBestEasy(localBigBoard) + 1;
+  }
+
+  int box = getStartSmallBoard(localPlacement);
+  char localBoard[3][3];
+  for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        localBoard[i][j] = board[box / 9 + i][box % 9 + j];
+
+  int localBox = findBestEasy(localBoard);
+  placeValue(player, box + ((localBox / 3) * 9 + localBox % 3));
+}
+
+void makeMoveAI() {
+  if (difficulty == EASY)
+    searchMoveEasy();
+
+  player = (player == 'X' ? 'O' : 'X');
+}
+
+/*    SCREEN MANIPULATION     */
 void finishGame(bool draw, char winner) {
-  state = 3;
+  state = FIN;
+  hasWon = true;
   playSound(200, 200);
   tft.fillScreen(BLACK);
   tft.setTextSize(3);
@@ -290,6 +397,89 @@ void selectBox(uint16_t potentiometerValue) {
   tft.drawLine(xPos, yPos, xPos + 19, yPos, RED);
 }
 
+void drawBoard() {
+  tft.fillScreen(BLACK);
+  for (int p = 12; p < SCR_SIZE; p += 24) {
+    uint16_t color = RGBto565(128, 128, 128);
+    tft.drawLine(p, 12, p, 228, color);
+    tft.drawLine(12, p, 228, p, color);
+  }
+
+  for (int p = 12; p < SCR_SIZE; p += 72) {
+    tft.drawLine(p, 12, p, 228, WHITE);
+    tft.drawLine(12, p, 228, p, WHITE);
+  }
+}
+
+void drawWinnerSmall(int x, int y, char value) {
+  int xPos = 12 + x * 24;
+  int yPos = 12 + y * 24;
+  tft.fillRect(xPos + 1, yPos + 1, 71, 71, BLACK);
+
+  xPos += 8;
+  yPos += 5;
+  tft.setTextColor(GREEN);
+  if (value == 'X') {
+    tft.setCursor(xPos, yPos);
+    tft.println(value);
+    tft.setCursor(xPos + 48, yPos);
+    tft.println(value);
+    tft.setCursor(xPos + 24, yPos + 24);
+    tft.println(value);
+    tft.setCursor(xPos, yPos + 48);
+    tft.println(value);
+    tft.setCursor(xPos + 48, yPos + 48);
+    tft.println(value);
+  } else {
+    tft.setCursor(xPos, yPos);
+    tft.println(value);
+    tft.setCursor(xPos + 24, yPos);
+    tft.println(value);
+    tft.setCursor(xPos + 48, yPos);
+    tft.println(value);
+    tft.setCursor(xPos, yPos + 24);
+    tft.println(value);
+    tft.setCursor(xPos + 48, yPos + 24);
+    tft.println(value);
+    tft.setCursor(xPos, yPos + 48);
+    tft.println(value);
+    tft.setCursor(xPos + 24, yPos + 48);
+    tft.println(value);
+    tft.setCursor(xPos + 48, yPos + 48);
+    tft.println(value);
+  }
+
+  tft.setTextColor(WHITE);
+}
+
+void drawMenu() {
+  state = MENU;
+  tft.setTextSize(3);
+  tft.fillScreen(BLACK);
+
+  tft.setCursor(10, 95);
+  tft.println("Singleplayer");
+  tft.setCursor(10, 125);
+  tft.println("Multiplayer");
+
+  tft.setTextSize(2);
+}
+
+void drawSingleplayerMenu() {
+  state = SP_MENU;
+  gameMode = SINGLE;
+  tft.setTextSize(3);
+  tft.fillScreen(BLACK);
+
+  tft.setCursor(10, 95);
+  tft.println("Easy");
+  tft.setCursor(10, 125);
+  tft.println("Hard");
+
+  tft.setTextSize(2);
+}
+
+/*    ARDUINO     */
 void initADC() {
   ADMUX = 0;
   ADMUX |= (1 << REFS0);
@@ -346,36 +536,6 @@ void playSound(uint8_t compareValue, int stopCount) {
   sei();
 }
 
-void startGame() {
-  currentBox = -1;
-  placement = ALL;
-  player = 'X';
-  moveCount = 0;
-
-  for (int i = 0; i < 9; i++)
-    for (int j = 0; j < 9; j++)
-      board[i][j] = '_';
-
-  for (int i = 0; i < 3; i++)
-    for (int j = 0; j < 3; j++)
-      bigBoard[i][j] = '_';
-  
-  drawBoard();
-}
-
-void drawMenu() {
-  state = 0;
-  tft.setTextSize(3);
-  tft.fillScreen(BLACK);
-
-  tft.setCursor(10, 95);
-  tft.println("Singleplayer");
-  tft.setCursor(10, 125);
-  tft.println("Multiplayer");
-
-  tft.setTextSize(2);
-}
-
 void setup() {
   Serial.begin(9600);
   DDRB |= (1 << PB1);
@@ -388,6 +548,6 @@ void setup() {
 }
 
 void loop() {
-  if (state == 2)
+  if (state == GAME)
     selectBox(readADC());
 }
